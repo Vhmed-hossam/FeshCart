da58dd52c1d03d700cf0be189871c03a503de285
Vhmed <vhmdhoss@gmail.com>
2025-06-09 20:49:32 +0300

    removing heroui spinner and adding a custom one

--- src/Components/Loading/loading.jsx
+++ src/Components/Loading/loading.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Spinner } from "@heroui/react";
 
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center h-screen ">
-      <Spinner size="lg" />
+    <div className="flex justify-center items-center h-screen">
+      <div class="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-[#2A7DDD] rounded-full">
+        <div class="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-b-red-500 rounded-full"></div>
+      </div>
     </div>
   );
 }
